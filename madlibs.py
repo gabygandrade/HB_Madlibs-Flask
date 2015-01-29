ab6d8207fe3dbfaf0dd277870004c23c55d24084
Hackbright Student <info@hackbrightacademy.com>
2015-01-29 15:54:28 -0800

    Finished 'GET' method

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,6 @@
 from random import choice
 from flask import Flask, render_template, request
 
-
 # "__name__" is a special Python variable for the name of the current module; Flask wants
 # to know this to know what any imported things are relative to.
 app = Flask(__name__)
@@ -27,6 +26,30 @@ def greet_person():
     compliment = choice(AWESOMENESS)
 
     return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route('/game')
+def show_game_form():
+    response = request.args.get("answer")
+
+    if response == "no":
+        return render_template("goodbye.html")
+    if response == "yes":
+        return render_template("game.html")
+
+@app.route('/madlib')
+def show_madlib():
+    person = request.args.get("person")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    color = request.args.get("color")
+    
+    POSSIBLE_MADLIBS = ["madlib.html", "madlib2.html"]
+
+    return render_template(choice(POSSIBLE_MADLIBS), 
+        person=person, 
+        noun=noun, 
+        adjective=adjective, 
+        color=color)
 
 
 if __name__ == '__main__':
